2877d0ae35bb7eacb401e95480581dd4f10760f3
Kenearos <kenearos@googlemail.com>
2026-05-12 00:12:22 +0200

    feat: add variants.js skeleton and wire into index.html + test.html

---
+++ variants.js
@@ -0,0 +1,33 @@
+/**
+ * Bonus-Varianten (NRW Psychiatrie 2011)
+ * Pure functions: day classification + V1/V2/V3 evaluation.
+ * Loaded after holidays.js and before calculator.js.
+ */
+
+// Will be implemented in subsequent tasks.
+function classify(date, holidayProvider) {
+    throw new Error('classify: not implemented');
+}
+
+function classifyDuties(duties, holidayProvider) {
+    throw new Error('classifyDuties: not implemented');
+}
+
+function variant1(classified, isVacation) {
+    throw new Error('variant1: not implemented');
+}
+
+function variant2(classified, isVacation) {
+    throw new Error('variant2: not implemented');
+}
+
+function variant3(classified, isVacation) {
+    throw new Error('variant3: not implemented');
+}
+
+// Expose globally
+window.classify = classify;
+window.classifyDuties = classifyDuties;
+window.variant1 = variant1;
+window.variant2 = variant2;
+window.variant3 = variant3;
